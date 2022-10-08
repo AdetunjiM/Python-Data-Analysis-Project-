c51c90e6d0856789b6414155b5f9f66d3371a74c
Adetunji Mofobi <czrotot999@yahoo.com>
2022-10-08 17:40:21 -0500

    main file updated with the discussed requirement for data generation

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
+import pandas as pd
+import numpy as np 
 
+#import libraries 
+#import faker and UUID
+
+"""
+    The columns to be created are ;
+    ID(unique to each customer ) 100 (using UUID)
+    First Name(Faker generator)
+    Last Name
+    Product (10 products )( create a dictionary )
+    Product Price( random price tied to each product)
+    Quantity of product (random between 1-10 pcs)
+    Total price (product of price and quantity)
+    
+"""
+#create header of master list using above specified columns 
+#master_list=[] #list containing all the data 
+
+
+#path ="c:/users/mofob/onedrive/desktop/datas/Python-Data-Analysis-Project-" ----> specify your path on local machine 
+#import libraries 
+
+#Dictionary of products as key and prices as value : or we can have a csv file with  product info
+#that has the product name and price , so we will need to read a file and take from the file 
+
+
+
+
+#def create_lst():
+    #loop through number of record needed in the master list  and call desired functions 
+    
+    #generate ID  called the fxn and save the output to a variable
+    #generate First Name and Last Name 
+    #generate the product name from the product dictionary or csv file ( and the get the )
+    #generte quantity of product bought  using random.randint 
+    #generate  total price using the function that multiplies qty and price 
+    #
+    #append this to a master list [ ] / specify order in master list 
+    #
+    #write each line to a csv file thats going to have the records 
+    #open the location csv file 
+    # write into the csv file  file_obj.writer(master list )
+    pass
+
+
+#create_lsit() ----> call the create list functionn finally 
+
+
+#####DATA GENERATION SECTION #######
+
+###vvvvvvvvvvvvvvvvvvvvvvvvvvvv###
+#DATA ANALYSIS WITH R OR PANDAS OR SPARK 
